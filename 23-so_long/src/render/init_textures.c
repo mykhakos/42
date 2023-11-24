0de154c0bc779297d8374538932ba9d474d29ee4
Konstantin Mykhailov <kmykhail@c2r5s3.42prague.com>
2023-11-24 15:12:28 +0100

    added to textures to the game functions. Everything works, but player cannot spen on closed exit

--- 23-so_long/src/render/init_textures.c
+++ 23-so_long/src/render/init_textures.c
@@ -6,7 +6,7 @@
 /*   By: kmykhail <kmykhail@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 20:22:00 by kmykhail          #+#    #+#             */
-/*   Updated: 2023/11/23 22:23:16 by kmykhail         ###   ########.fr       */
+/*   Updated: 2023/11/24 15:10:55 by kmykhail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static void	init_texture_player(t_so_long *so_long)
 {
 	(*so_long).textures.player_up.img_ptr = mlx_xpm_file_to_image(
 			(*so_long).mlx.mlx,
-			"./textures/player.xpm",
+			"./textures/player_up.xpm",
 			&(*so_long).textures.player_up.img_width,
 			&(*so_long).textures.player_up.img_height);
 	(*so_long).textures.player_down.img_ptr = mlx_xpm_file_to_image(
@@ -26,12 +26,12 @@ static void	init_texture_player(t_so_long *so_long)
 			&(*so_long).textures.player_down.img_height);
 	(*so_long).textures.player_left.img_ptr = mlx_xpm_file_to_image(
 			(*so_long).mlx.mlx,
-			"./textures/player.xpm",
+			"./textures/player_left.xpm",
 			&(*so_long).textures.player_left.img_width,
 			&(*so_long).textures.player_left.img_height);
 	(*so_long).textures.player_right.img_ptr = mlx_xpm_file_to_image(
 			(*so_long).mlx.mlx,
-			"./textures/player.xpm",
+			"./textures/player_right.xpm",
 			&(*so_long).textures.player_right.img_width,
 			&(*so_long).textures.player_right.img_height);
 }
@@ -40,12 +40,12 @@ static void	init_texture_exit(t_so_long *so_long)
 {
 	(*so_long).textures.exit_closed.img_ptr = mlx_xpm_file_to_image(
 			(*so_long).mlx.mlx,
-			"./textures/exit.xpm",
+			"./textures/exit_closed.xpm",
 			&(*so_long).textures.exit_closed.img_width,
 			&(*so_long).textures.exit_closed.img_height);
 	(*so_long).textures.exit_open.img_ptr = mlx_xpm_file_to_image(
 			(*so_long).mlx.mlx,
-			"./textures/collectible.xpm",
+			"./textures/exit_open.xpm",
 			&(*so_long).textures.exit_open.img_width,
 			&(*so_long).textures.exit_open.img_height);
 }
@@ -54,7 +54,7 @@ void	init_textures(t_so_long *so_long)
 {
 	(*so_long).textures.floor.img_ptr = mlx_xpm_file_to_image(
 			(*so_long).mlx.mlx,
-			"./textures/black.xpm",
+			"./textures/floor.xpm",
 			&(*so_long).textures.floor.img_width,
 			&(*so_long).textures.floor.img_height);
 	(*so_long).textures.wall.img_ptr = mlx_xpm_file_to_image(
